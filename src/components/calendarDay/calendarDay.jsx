37bf5193d4c2d086c5b81e98d1f4ecdf47342c54
isakhalin <47768954+isakhalin@users.noreply.github.com>
2023-01-11 15:56:59 +1100

    refactoring

--- src/components/calendarDay/calendarDay.jsx
+++ src/components/calendarDay/calendarDay.jsx
@@ -9,33 +9,14 @@ import {VksConstructor} from "../vksConstructor";
 export const CalendarDay = ({daySelected}) => { // В daySelected приходит дата в милисекундах
   const [dayStartAt, setDayStartAt] = useState("09:00:00");
   const [dayEndAt, setDayEndAt] = useState("21:00:00")
-
-  // console.log("Пришло в календарь", new Date(daySelected).toLocaleDateString().split("."));
-  //console.log("!!!", new Date("2023-01-01 00:00:00").getTime())
-
-  const calcDayStart = () => {
-    const date = new Date(daySelected).toLocaleDateString().split(".");
-    return new Date(`${date[2]}-${date[1]}-${date[0]} ${dayStartAt}`).getTime();
-  }
-
-  const calcDayEnd = () => {
-    const date = new Date(daySelected).toLocaleDateString().split(".");
-    return new Date(`${date[2]}-${date[1]}-${date[0]} ${dayEndAt}`).getTime();
-  }
-
-  // console.log("Data", new Date(daySelected).getDay() + 1)
-  // console.log("!!!!", calcDayStart());
-  // console.log("!!!!", calcDayEnd());
-
+  // TODO Заменить на данные из редюсера
   const [vks, setVks] = useState([
     {
       id: "434264361",
       title: "new",
       description: "",
-      // dayStartAt: new Date("2023-02-01 09:00:00").getTime(),
-      // dayEndAt: new Date("2023-02-01 21:00:00").getTime(),
-      start: new Date("2023-02-01 09:30:00").getTime(),
-      end: new Date("2023-02-01 10:00:00").getTime(),
+      start: 1675204200000, // new Date("2023-02-01 09:30:00").getTime()
+      end: 1675206000000,   // new Date("2023-02-01 10:00:00").getTime()
       styles: {
         top: 0,
         height: "5%",
@@ -45,10 +26,8 @@ export const CalendarDay = ({daySelected}) => { // В daySelected приходи
       id: "43467345361",
       title: "Вопросы общего образования образования образования образования образования образования",
       description: "",
-      // dayStartAt: new Date("2022-12-20 09:00:00").getTime(),
-      // dayEndAt: new Date("2022-12-20 21:00:00").getTime(),
-      start: new Date("2023-02-01 10:30:00").getTime(), // 1671523200000
-      end: new Date("2023-02-01 11:45:00").getTime(), // 1671526800000
+      start: 1675207800000, // new Date("2023-02-01 10:30:00").getTime()
+      end: 1675212300000,   // Date("2023-02-01 11:45:00").getTime()
       styles: {
         top: "15%", // 1 минута это 0,166666%. Рассчет для пикселей: 30 минут это 30px (start-dayStartAt)/60000
         height: "12.5%", // Рассчет для процентов 0.166666667 * мин. Рассчет для пикселей: (end-start)/60000
@@ -56,15 +35,7 @@ export const CalendarDay = ({daySelected}) => { // В daySelected приходи
     },
   ])
 
-  const commonStyles = {
-    position: "absolute",
-    backgroundColor: "#ed8550",
-    width: "100%",
-    borderBottom: "1px solid black",
-    boxSizing: "border-box",
-  }
-
-  const [day, setDay] = useState([
+  const day = [
     {
       title: "9:00",
     },
@@ -143,9 +114,24 @@ export const CalendarDay = ({daySelected}) => { // В daySelected приходи
     // {
     //   title: "21:30",
     // },
-  ])
+  ];
+  const commonStyles = {
+    position: "absolute",
+    backgroundColor: "#ed8550",
+    width: "100%",
+    borderBottom: "1px solid black",
+    boxSizing: "border-box",
+  }
 
-  // console.log("randomizer", Math.floor((Math.random() * 1000000000)))
+  const calcDayStart = () => {
+    const date = new Date(daySelected).toLocaleDateString().split(".");
+    return new Date(`${date[2]}-${date[1]}-${date[0]} ${dayStartAt}`).getTime();
+  }
+
+  const calcDayEnd = () => {
+    const date = new Date(daySelected).toLocaleDateString().split(".");
+    return new Date(`${date[2]}-${date[1]}-${date[0]} ${dayEndAt}`).getTime();
+  }
 
   return (
     <div>
@@ -169,7 +155,7 @@ export const CalendarDay = ({daySelected}) => { // В daySelected приходи
           </div>
         ))}
       </div>
-      <VksConstructor vks={vks} setVks={setVks} calcDayStart={calcDayStart()}/>
+      <VksConstructor vks={vks} setVks={setVks}/>
     </div>
   );
 };
